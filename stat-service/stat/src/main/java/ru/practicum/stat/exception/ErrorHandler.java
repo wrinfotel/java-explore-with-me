773b59bc429c2ae92d9e47f36d168bcee3619e56
Vladimir <wrinfotel@mail.ru>
2025-05-27 16:46:07 +0300

    добавлены обработчики исключений

--- stat-service/stat/src/main/java/ru/practicum/stat/exception/ErrorHandler.java
+++ stat-service/stat/src/main/java/ru/practicum/stat/exception/ErrorHandler.java
@@ -1,7 +1,6 @@
 package ru.practicum.stat.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
